ed0308ee1d6a3ea620e37fce226b826ee9927f20
jamppajoo <mummo94@hotmail.com>
2016-09-13 18:53:29 +0300

    Made levelpack 3 with kindof working teleport system

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+    private GameObject upperTeleport;
+    private GameObject Blop;
+
+	// Use this for initialization
+	void Start () {
+        Blop = GameObject.Find("Blop");
+        upperTeleport = GameObject.Find("UpperTeleport");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("EASPDOK");
+        if (c.gameObject.name == "Blop")
+        {
+            c.transform.position = new Vector3(c.transform.position.x, upperTeleport.transform.position.y, c.transform.position.z);
+        }
+    }
+}
